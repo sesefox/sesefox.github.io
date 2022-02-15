3de65ec27e970787c30cbdb90817fd1e339e7dd0
sesefox <99642537+sesefox@users.noreply.github.com>
2022-02-15 12:19:16 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,65 @@
+var fabuye={
+	
+    'github发布页:': {
+		'link':'https://sesefox.github.io'
+		},
+    'bitbucket发布页:': {
+		'link':'https://sexfox.xyz'
+		}	
+		
+};
+var links={
+	
+    '最新地址': {
+		'link':'https://sexfox1.xyz'
+		},
+    '备用地址': {
+		'link':'https://sexfox.xyz'
+		},
+		
+};
+var foot={
+
+    '1': {
+		'txt':'感谢网友们的支持，发布页可以获取最新地址'
+		},
+    '2': {
+		'txt':'色狐狸永久地址:sexfox.xyz(海外用户或科学上网用户可进行访问）'
+		},
+    '3': {
+		'txt':'如遇到所有地址无法访问,请写邮件至<a class="youjian" href="quisade59@gmail.com" target="_blank">quisade59@gmail.com</a>即可获取最新消息'
+		},		
+};
+var pcimg=8;
+var wapimg=13;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
