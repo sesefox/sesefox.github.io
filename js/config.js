fcba01abc4f06f0eab136fcb2b1e9faf5a6c198f
sesefox <99642537+sesefox@users.noreply.github.com>
2022-02-16 00:49:30 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
+var title ='色狐狸,发布页,最新发布页,福利发布页，宅男最爱发布页';
 var fabuye={
 	
     'github发布页:': {
 		'link':'https://sesefox.github.io'
 		},
     'bitbucket发布页:': {
-		'link':'正在添加中'
+		'link':'正在制作中！'
 		}	
 		
 };
@@ -27,11 +28,14 @@ var foot={
 		'txt':'色狐狸永久地址:sexfox.xyz(海外用户或科学上网用户可进行访问）'
 		},
     '3': {
-		'txt':'如遇到所有地址无法访问,请写邮件至<a class="youjian" href="quisade59@gmail.com" target="_blank">quisade59@gmail.com</a>即可获取最新消息'
+		'txt':'如遇到所有地址无法访问,请耐心等待，更换网址可能需要点时间！'
 		},		
 };
+//图片数量
 var pcimg=8;
 var wapimg=13;
+//下方临时文件
+var temp='请写邮件至<a class="youjian" href="quisade59@gmail.com" target="_blank">quisade59@gmail.com</a>即可获取最新消息';
 
 
 
