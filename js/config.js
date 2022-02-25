a4f085b34df776cf8fb9930a799b7a6d04791b71
sesefox <99642537+sesefox@users.noreply.github.com>
2022-02-25 16:19:05 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -31,8 +31,5 @@ var foot={
 		'txt':'如遇到所有地址无法访问,请耐心等待，更换网址可能需要点时间！'
 		},		
 };
-//图片数量
-var pcimg=8;
-var wapimg=13;
 //下方临时文件
 var temp='请写邮件至<a class="youjian" href="quisade59@gmail.com" target="_blank">quisade59@gmail.com</a>即可获取最新消息';
